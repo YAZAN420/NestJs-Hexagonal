308e991688ca7954ad423800ca07ec44f85ac644
Yazan420 <yazanmahfooz8@gmail.com>
2026-03-28 01:51:16 +0300

    ..

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { GlobalExceptionFilter } from './common/presentation/filters/global-exce
 
 async function bootstrap() {
   const app = await NestFactory.create(
-    AppModule.register({ driver: 'mongoose' }),
+    AppModule.register({ driver: 'in-memory' }),
     {
       logger: WinstonModule.createLogger(winstonConfig),
     },
